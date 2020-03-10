5470a3be52a729d225beba73fad4d29bd67fc1de
Kevin Auer <kvn35124@uab.edu>
2020-03-10 11:21:24 -0500

    making models for components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'Test Description', 'https://www.pinkwhen.com/wp-content/uploads/2015/08/chicken-1.jpg')
+  ];
 
   constructor() { }
 
